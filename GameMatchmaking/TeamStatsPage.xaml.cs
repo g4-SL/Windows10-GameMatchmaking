c016f2cbcfeafb19d88302cad1cf409915fe60af
g4-SL <slouise@live.com.my>
2015-05-17 08:02:27 -0400

    Changed select sports layout

--- GameMatchmaking/TeamStatsPage.xaml.cs
+++ GameMatchmaking/TeamStatsPage.xaml.cs
@@ -2,16 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,15 +19,67 @@ namespace GameMatchmaking
     /// </summary>
     public sealed partial class TeamStatsPage : Page
     {
+        private String txtNumWinCode = "";
+        private String txtNumLossCode = "";
+        private String txtRankCityCode = "";
+        private String txtRankCountryCode = "";
+        private String txtPointsRatioCode = "";
+        private String txtPlayersCode = "";
+        private String txtSportTypeCode = "";
+        private String txtNumTiesCode = "";
 
         public TeamStatsPage()
         {
             this.InitializeComponent();
+            getTeamInfo();
+            txtNumWin.Text = txtNumWinCode;
+            txtNumLoss.Text = txtNumLossCode;
+          //  txtRankCity.Text = txtRankCityCode;
+          //  txtRankCountry.Text = txtRankCountryCode;
+            txtPointsRatio.Text = txtPointsRatioCode;
+            txtPlayers.Text = txtPlayersCode;
+            txtSportType.Text = txtSportTypeCode;
+            txtNumTies.Text = txtNumTiesCode;
+        }
 
-            txtNumWin.Text = "1";
-            txtNumLoss.Text = "1";
-            txtRankCity.Text = "1";
-            txtRankCountry.Text = "1";
+        async private void getTeamInfo()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Config.URI);
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("api/team/team/");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = await response.Content.ReadAsStringAsync();
+
+                        D.p(result);
+
+                        JsonObject res = JsonObject.Parse(result);
+                        txtNumWinCode = res.GetNamedString("wins");
+                        txtNumLossCode = res.GetNamedString("losses");
+                        //txtRankCityCode = res.GetNamedString("status");
+                       // txtRankCountryCode = res.GetNamedString("status");
+                        txtPointsRatioCode = res.GetNamedString("points_ratio");
+                        txtPlayersCode = res.GetNamedString("usernames");
+                        txtSportTypeCode = res.GetNamedString("sport");
+                        txtNumTiesCode = res.GetNamedString("ties");
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+
+
+        private void onCancelClick(object sender, RoutedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            rootFrame.Navigate(typeof(HomePage));
+
         }
     }
 }
