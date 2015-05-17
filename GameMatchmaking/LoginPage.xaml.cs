c10fc548859402d5d3627670e9572a9758182028
4ngelaZhou <angela.kaisy@hotmail.com>
2015-05-16 21:31:45 -0400

    Added Login Page Template

---
+++ GameMatchmaking/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace GameMatchmaking
+{
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            //this.InitializeComponent();
+        }
+
+        private void sign_up_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
